d0be194f0a8059cbeffd393efbc16555ca310744
Malak Zangar <64647127+malak-zangar@users.noreply.github.com>
2024-09-12 23:48:26 +0100

    Update document.ts

--- src/app/GestionDoc/document.ts
+++ src/app/GestionDoc/document.ts
@@ -2,13 +2,7 @@ import { Byte } from "@angular/compiler/src/util";
 import { Tag } from "./tag";
 
 export class Document {
-   /* id: number;
-    titre: string;
-    type:string;
-    data:Byte[];
-    tags:Tag[];
-    dep:string;
-    tag: Set<string>;*/
+
     id: number;
     name: string;
     contentType:string;
